8b63aaea2ef72630cf091f3df901d706376be4de
DESKTOP-GRJIQGI\e <truongminhtriet96@gmail.com>
2017-10-16 13:32:37 +0700

    first commit

---
+++ FAHASA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FAHASA.Startup))]
+namespace FAHASA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
